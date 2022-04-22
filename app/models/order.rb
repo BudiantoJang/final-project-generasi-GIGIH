10bcb35d18f2e077b5413e4ab8afe7fa9b5b5a54
Budianto <budiantojang@gmail.com>
2022-04-22 23:52:37 +0700

    added link to make new item in item index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
     has_many :order_details, :dependent => :delete_all 
     belongs_to :customer
+
+
+    def self.revenue
+        total_revenue = 0
+        self.all.each do |order|
+            total_revenue += order.final_price
+        end
+        return total_revenue
+    end
 end
