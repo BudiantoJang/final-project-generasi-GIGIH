8a711ed42666041e3aa04dd4dd84919f61784c4d
Budianto <budiantojang@gmail.com>
2022-04-22 23:13:11 +0700

    added category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
     has_many :item_categories, :dependent => :delete_all 
     has_many :items, through: :item_categories
+
+    #presence = can't be blank
+    #uniqueness = can't have duplicate values
+    validates :name, {presence: true, uniqueness: true}
 end
