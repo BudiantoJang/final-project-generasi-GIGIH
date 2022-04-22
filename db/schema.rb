1586462a457c254e9872b418539c8338d9f60d29
Budianto <budiantojang@gmail.com>
2022-04-22 22:55:31 +0700

    add status column into order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_155351) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_155459) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_22_155351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
+    t.string "status"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
