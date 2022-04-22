efb85ec542167bd4685f914ca4e3ffca17e34196
Budianto <budiantojang@gmail.com>
2022-04-22 23:14:43 +0700

    added customer validations and changed relations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ApplicationRecord
-    has_many :item_categories, :dependent => :delete_all 
-    has_many :items, through: :item_categories
+    has_many :orders
+
+    #presence = can't be blank
+    #uniqueness = can't have duplicate values
+    #format = determining valid email formatting
+    validates:name, presence: true
+    validates:email, {presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } }
 end
