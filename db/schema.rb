8fc07a731f1e38618af4f1028ab5828dd2bbc926
Budianto <budiantojang@gmail.com>
2022-04-22 22:53:28 +0700

    created customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_22_155206) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_22_155316) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
